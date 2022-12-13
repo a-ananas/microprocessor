86d6baeebfd638d1dca0a355635c6b6ff4f3f885
MrBigoudi <yannis.kedadry2@gmail.com>
2022-12-13 23:19:14 +0100

    nulln(a) -> 1 if a is null + eqn(a,b) -> 1 if a==b

--- proc_netlist/alu/comp.py
+++ proc_netlist/alu/comp.py
@@ -1,36 +1,34 @@
 from lib_carotte import *
-from const import *
+from logic import xorn 
 
-def is_null_step_n(a: Variable, n:int) -> Variable :
-    assert(a.bus_size == n)
-    tmp = 0
-    if n == 32:
-        tmp = EquationVariable(16)
-    elif n == 16:
-        tmp = EquationVariable(8)
-    elif n == 8:
-        tmp = EquationVariable(4)
-    elif n == 4:
-        tmp = EquationVariable(2)
-    else:
-        assert(False)
-
-    for i in range(n//2):
-        tmp[i] = (a[2*i] | a[2*i+1])
-
+def is_null_step(a: Variable) -> Variable :
+    assert(a.bus_size > 2)
+    assert(a.bus_size % 2 == 0)
+    n = a.bus_size
+    tmp = a[0] | a[1]
+    for i in range(1,n//2):
+        print(i)
+        tmp = tmp + (a[2*i] | a[2*i+1])
+    assert(tmp.bus_size == (n//2))
     return tmp
 
 
-# is_null -> not(a[0] | a[1] | ... a[31])
-def is_null(a: Variable) -> Variable :
-    assert(a.bus_size == 32)
-    tmp = is_null_step_n(a, 32)
-    assert(tmp.bus_size == 16)
-    tmp = is_null_step_n(tmp, 16)
-    assert(tmp.bus_size == 8)
-    tmp = is_null_step_n(tmp, 8)
-    assert(tmp.bus_size == 4)
-    tmp = is_null_step_n(tmp, 4)
+# null -> not(a[0] | a[1] | ... a[31])
+def nulln(a: Variable) -> Variable :
+    n = a.bus_size
+    if n == 1:
+        return ~a
+    if n == 2:
+        return ~(a[0] | a[1])
+    assert(a.bus_size%2 == 0)
+    tmp = is_null_step(a)
+    while tmp.bus_size > 2:
+        tmp = is_null_step(tmp)
     assert(tmp.bus_size == 2)
-    ~(tmp[0] | tmp[1])
-    
+    return ~(tmp[0] | tmp[1])
+
+
+# a == b -> not( (a[0] xor b[0]) + (a[1] xor b[1]) + ... )) == 1
+def eqn(a: Variable, b: Variable) -> Variable :
+    assert(a.bus_size == b.bus_size)
+    return nulln(xorn(a, b))
