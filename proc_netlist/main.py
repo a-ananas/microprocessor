3699a8d6b3a252fd1ccb1b93abfb5b5093cf160a
MrBigoudi <yannis.kedadry2@gmail.com>
2022-12-13 16:03:34 +0100

    make working

--- proc_netlist/main.py
+++ proc_netlist/main.py
@@ -1,20 +1,14 @@
 from lib_carotte import *
 import sys
 sys.path.append("./alu")
-<<<<<<< HEAD
-from shift import *
-=======
+
 import shift
->>>>>>> 967170e014093dc4424652df054c89d4c4cfc20a
+import arith
 
 def main() -> None:
     '''Entry point of this example'''
-    a = Input(8)
-<<<<<<< HEAD
-#    b = Input(5)
-    r = sllk(a,2)
+    a = Input(4)
+    b = Input(4)
+    (r,c) = arith.addn(a,b)
     r.set_as_output("r")
-=======
-    r = shift.sllk(a, 2)
-    r.set_as_output("r")
->>>>>>> 967170e014093dc4424652df054c89d4c4cfc20a
+    c.set_as_output("c")
