0e2de9ee7b8245c007d82405c8d6ba86e05d7cca
MrBigoudi <yannis.kedadry2@gmail.com>
2022-12-14 00:05:55 +0100

    skeletton for alu file

---
+++ proc_netlist/alu/alu.py
@@ -0,0 +1,13 @@
+from lib_carotte import *
+
+import arith
+import comp
+import const
+import logic
+import utils
+import shift
+
+# the main alu
+def alu(instr: Variable) -> Variable:
+    #todo
+    assert(False)
