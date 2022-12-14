b9a490ef26bf03f8e25b384d6ee89f7b82bdc0dc
MrBigoudi <yannis.kedadry2@gmail.com>
2022-12-14 14:02:50 +0100

    update report opcode

--- proc_netlist/alu/alu.py
+++ proc_netlist/alu/alu.py
@@ -9,5 +9,11 @@ import shift
 
 # the main alu
 def alu(instr: Variable) -> Variable:
-    #todo
+    # get opcode
+    assert(instr.bus_size == 32)
+    opcode = instr[0:5]
+
+    # get values according to opcode
+    # do the operation according to opcode
+    # return the result
     assert(False)
