9d21c7e6ce6902b72059d2fddb59a3c386c3c296
MrBigoudi <yannis.kedadry2@gmail.com>
2022-12-14 14:28:28 +0100

    get format from opcode

--- proc_netlist/alu/alu.py
+++ proc_netlist/alu/alu.py
@@ -7,6 +7,20 @@ import logic
 import utils
 import shift
 
+# return the format of an operation from an opcode
+def get_format(opcode: Variable) -> Variable:
+    #             s0 s1
+    # format R ->  0  0
+    # format U ->  1  0
+    # format I ->  0  1
+    assert(opcode.bus_size == 5)
+    s1 = (~opcode[3]) | (opcode[4] & opcode[2] & opcode[1] & opcode[0])
+    s0 = opcode[4] & opcode[3] & (~opcode[2])
+    format = s0 + s1
+    assert(format.bus_size == 2)
+    return s0+s1 
+
+
 # the main alu
 def alu(instr: Variable) -> Variable:
     # get opcode
@@ -14,6 +28,8 @@ def alu(instr: Variable) -> Variable:
     opcode = instr[0:5]
 
     # get values according to opcode
+    format = get_format(opcode)
+
     # do the operation according to opcode
     # return the result
-    assert(False)
+    return format
