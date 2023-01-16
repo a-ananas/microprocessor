bd9339262d7f1fb64c8868f2a30f4182a56881d6
Oscar Garnier <oscar.garnier@gmail.com>
2023-01-16 11:37:38 +0100

    decodeur fini

--- proc_netlist/decodeur/decodeur.py
+++ proc_netlist/decodeur/decodeur.py
@@ -3,7 +3,7 @@
 #et une sortie avec absolument tout, a voir
 from lib_carotte import *
 
-import utils
+#import utils
 
 
 def decodeur_dinstructions(instr: Variable):
@@ -14,8 +14,8 @@ def decodeur_dinstructions(instr: Variable):
     isI = get_isI(opcode)
     isU = get_isU(opcode)
     #set defaults
-    immD = Constant(32 * '0') #immediat saur 32 bits
-    Wenable = Constant('0')
+    immD = Constant(32 * '0') #immediat sur 32 bits
+    WenableD = Constant('0')
 
     rd = instr[5:10]
     rs1 = instr[10:15]
@@ -23,15 +23,19 @@ def decodeur_dinstructions(instr: Variable):
  
     #get for R
     
-    immR,WenableR = get_field_R(opcode)
-    immI,WenableI = get_field_I(opcode)
-    immU,WenableU = get_field_U(opcode)
+    immR,WenableR = get_field_R(instr)
+    immI,WenableI = get_field_I(instr)
+    immU,WenableU = get_field_U(instr)
 
     imm = Mux(isU,Mux(isI,Mux(isR,immD,immR),immI),immU)
+
     Wenable = Mux(isU,Mux(isI,Mux(isR,WenableD,WenableR),WenableI),WenableU)
 
-    return opcode,imm,rs1,rs2,rd,Wenable
+    return opcode,imm,rs1,rs2,rd,Wenable,isR,isI,isU,opcode[0],opcode[1],opcode[2],opcode[3],opcode[4]
 
+
+def test(o: Variable ):
+    return o[0]
 #Note that the optcode table is written  in human!  The first number read is
 #the heavy digit, so it corresponds to optcode[4]
 
@@ -42,8 +46,8 @@ def get_isI(o: Variable):
     c = o[4] & ~(o[3] | o[2] | o[1] | o[0])
     d = o[4] & o[2] & o[1] & ~o[3] & ~o[0]
     #le code 11111 est de type I aiussi
-    b = And5(o[0],o[1],o[2],o[3],o[4])
-    return a | b | c | d
+    b = o[0] & o[1] & o[2] & o[3] & o[4]
+    return (a | b | c | d) & ~(o[0] & ~o[1] & ~o[2])
 
 def get_isR(o: Variable):
     assert o.bus_size == 5
@@ -51,8 +55,9 @@ def get_isR(o: Variable):
     a = And(Not(o[4]),o[3])
     #le code 11110
     b = o[4] & o[3] & o[2] & o[1] & ~o[0]
-    #les codes 10 + tout sauf 000 et 110 
-    c = ~o[4] & o[3] & ((o[2] | o[1] | o[0]) & (~o[2] | ~o[1] | o[0]))  
+    #les codes 10 + tout sauAf 000 et 110 et 010 (celui la ne correspond a rien, donc c'est pas tres grave, mais je prefere que isR soit faux dans ce cas) 
+    cUnDesTrois = ~(o[2] | o[1] | o[0]) | (o[2] & o[1] & ~o[0]) | (~o[2] & o[1] & ~o[0])
+    c = o[4] & ~o[3] & ~cUnDesTrois
 
     return a | b | c
 
@@ -89,13 +94,20 @@ def get_field_I(instr: Variable) -> Variable:
 def get_field_R(instr: Variable) -> Variable:
     assert(instr.bus_size == 32)
     imm = Constant(32 * '0')
-    Wenable = 1 #all the R instruction are yes on wenable
+    Wenable = Constant("1") #all the R instruction are yes on wenable
     return (imm, Wenable)
 
-    
-def test(instr: Variable):
-    doit = Constant('0')
-    if Variable == Constant('00'):
-        doit = Constant('1')
+def testDeco2() -> None:
+    a = Input(32)
+    b = isR(a)
+    b.set_as_output("output")
 
-    return doit
+def testDeco() -> None:
+    a = Input(32)
+    opcode,imm,rs1,rs2,rd,Wenable,isR,isI,isU,q,w,e,r,t= decodeur_dinstructions(a) 
+    opcode.set_as_output("opcode")
+    imm.set_as_output("imm")
+    rs1.set_as_output("rs1")
+    rs2.set_as_output("rs2")
+    rd.set_as_output("rd")
+    Wenable.set_as_output("Wenable")
