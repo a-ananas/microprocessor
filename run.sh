d8a3901f23e0b992528d93f3e1ecb38899ea54e0
MrBigoudi <yannis.kedadry2@gmail.com>
2023-01-18 13:53:01 +0100

    changing script for handling errors

--- run.sh
+++ run.sh
@@ -28,11 +28,11 @@ show_help(){
     echo "Usage: ./${0##*/} [OPTION]... [FILE]..."
     echo "Run a program written in our assembly language on our microprocessor"
     echo ""
-    printf "\t-h, -?, --help \tdisplay this help and exit\n"
-    printf "\t-n, --nb-step \tthe number of steps to run the simulator\n"
+    printf "\t-h, -?, --help \t\tdisplay this help and exit\n"
+    printf "\t-n, --nb-step \t\tthe number of steps to run the simulator\n"
     printf "\t-c, --clk, --clock \tthe format the output as a formated 7-segment using values from registers x0 to x13\n\n"
     echo "Examples"
-    printf "\t./run.sh ./assembler/tests/test_ram.x \t Run the program ./assembler/tests/test_ram.x infinitely\n"
+    printf "\t./run.sh ./assembler/tests/test_ram.x \t\t Run the program ./assembler/tests/test_ram.x infinitely\n"
     printf "\t./run.sh -n 11 ./assembler/tests/test_ram.x \t Run the program ./assembler/tests/test_ram.x during 11 simulator steps\n"
     printf "\t./run.sh --clk ./assembler/tests/test_7seg.x \t Run the program ./assembler/tests/test_7seg.x infinitely and format it as a 7-segment clock\n"
 }
@@ -76,7 +76,10 @@ done
 
 code="$1"
 
-[[ -f ${code} ]] || (show_help && exit 1);
+if [[ ! -f ${code} ]]; then
+    show_help 
+    exit 1
+fi
 ./${assembler} ${code};
 rom="${code%.x}.i";
 
